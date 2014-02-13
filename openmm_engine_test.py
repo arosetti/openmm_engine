b5d51f1df12af66f0368b13c6debfd28aead947d
parad0x <alessandro.rosetti@gmail.com>
2014-02-13 01:29:50 +0100

    added fake game-ui

--- openmm_engine_test.py
+++ openmm_engine_test.py
@@ -6,12 +6,22 @@ modified to work with python3
 it's an interesting example of use of openg with python
 '''
 
-import sys, math, numpy
+import io, sys, math, numpy
 from Lod import *
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from PIL import Image
+
+import threading, time
+
+sky_rot = 0.0
+
+def threadInc():
+    global sky_rot
+    while True:
+        time.sleep(.01)
+        sky_rot = (sky_rot + 0.007) % 360.0
 
 lm = 0
 window = 0
@@ -22,6 +32,8 @@ X_AXIS = 0.0
 Y_AXIS = 0.0
 Z_AXIS = 0.0
 
+
+
 DIRECTION = 1
 
 textureFloor  = 0
@@ -29,6 +41,14 @@ textureCeil  = 1
 textureWall = 2
 textureCube = 3
 textureSky = 4
+textureBorder1 = 5
+textureBorder2 = 6
+textureBorder3 = 7
+textureBorder4 = 8
+textureEradcate = 9
+textureMaleA01 = 10
+textureTap2 = 11
+textureFooter = 12
 
 angle = 0
 
@@ -68,17 +88,24 @@ image2 = ""
 def InitGL(Width, Height): 
     glClearColor(0.0, 0.0, 0.0, 0.0)
     glClearDepth(1.0) 
-    glDepthFunc(GL_LESS)
+    #glDepthFunc(GL_LESS)
+    glDepthFunc(GL_LEQUAL) 
     glEnable(GL_DEPTH_TEST)
     glShadeModel(GL_SMOOTH)   
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 200.0)
+    gluPerspective(50.0, float(Width)/float(Height), 0.1, 300.0)
     glMatrixMode(GL_MODELVIEW)
     glEnable(GL_TEXTURE_2D) # initialize texture mapping
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glEnable (GL_BLEND)
+    glDisable(GL_COLOR_MATERIAL)
+
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+
     
  
 def keyPressed(*args):
@@ -109,6 +136,7 @@ def keyPressed(*args):
 
 def DrawBox():
     glBindTexture(GL_TEXTURE_2D, textureCube);
+    glPushMatrix();
     glTranslatef(3.0,-.5,-3.0)
     glScaled(.4,.4,.4);
     glBegin(GL_QUADS);
@@ -137,11 +165,14 @@ def DrawBox():
     glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
     glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
     glEnd();
+    glPopMatrix();
 
 def DrawSky():
     glBindTexture(GL_TEXTURE_2D, textureSky);
-    glTranslatef(0,0,0)
     glScaled(200,200,200);
+    glPushMatrix();
+    glTranslatef(0,0,0)
+    glRotatef(sky_rot, 0.0, 1.0, 0.0);
     glBegin(GL_QUADS);
     glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
     glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
@@ -168,31 +199,14 @@ def DrawSky():
     glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
     glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
     glEnd();
+    glPopMatrix();
 
-def DrawGLScene():
-    global X_AXIS,Y_AXIS,Z_AXIS
-    global DIRECTION
- 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
- 
-    glLoadIdentity()
-
-    glTranslatef(0.0+camx,0.0,0.0+camz)
-    glTranslatef(0.0-camx,0.0,0.0-camz)
-    glRotatef(X_AXIS,1.0,0.0,0.0)
-    glRotatef(Y_AXIS,0.0,1.0,0.0)
-    glRotatef(Z_AXIS,0.0,0.0,1.0)
-    glTranslatef(0.0+camx,0.0,-0.0+camz)
-    #glBindTexture(GL_TEXTURE_2D, ID)
-    #glBindTexture(GL_TEXTURE_2D, texture)   # 2d texture (x and y size)
-
+def DrawDungeon():
     glPushMatrix();
     glTranslatef(-2.0,0.0,1.0)
-    #glBindTexture(GL_TEXTURE_2D, image)
     for numz in range(0,10):
         for num in range(0,10):
             glBindTexture(GL_TEXTURE_2D, textureFloor)   # 2d texture (x and y size)
-
             glBegin(GL_QUADS); # floor
             glTexCoord2f(1.0, 1.0); glVertex3f(-1.0 + num*2, -1.0, -1.0 - numz*2);
             glTexCoord2f(0.0, 1.0); glVertex3f( 1.0 + num*2, -1.0, -1.0 - numz*2);
@@ -209,9 +223,7 @@ def DrawGLScene():
             glEnd();
 
             glBindTexture(GL_TEXTURE_2D, textureWall)   # 2d texture (x and y size)
-
             if matrix[num][numz] == 1:
-                # Draw Cube (multiple quads)
                 glBegin(GL_QUADS);
                 glTexCoord2f(0.0, 0.0); glVertex3f(-1.0 + num*2, -1.0,  1.0 - numz*2);
                 glTexCoord2f(1.0, 0.0); glVertex3f( 1.0 + num*2, -1.0,  1.0 - numz*2);
@@ -239,32 +251,107 @@ def DrawGLScene():
                 glTexCoord2f(0.0, 1.0); glVertex3f(-1.0 + num*2,  1.0, -1.0 - numz*2);
                 glEnd();
     glPopMatrix();
-    DrawBox();
 
-    DrawSky();
-    # X_AXIS = X_AXIS - 0.30
-    # Z_AXIS = Z_AXIS - 0.30
-    glutSwapBuffers()
+def Set2DMode():
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    #gluOrtho2D(0.0, 640, 0.0, 480);
+    glOrtho(0, 640, 480, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glDisable(GL_DEPTH_TEST)
 
-def loadTexture (dirname, sfile, texture):
+def Draw2DImage(texture, w, h, x, y):
+    glBindTexture(GL_TEXTURE_2D, texture)
+    glPushMatrix();
+    glTranslatef(x,y,0);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 1.0); glVertex2f(0.0, 0.0); 
+    glTexCoord2f(0.0, 0.0); glVertex2f(0.0, h);
+    glTexCoord2f(1.0, 0.0); glVertex2f(w, h);
+    glTexCoord2f(1.0, 1.0); glVertex2f(w, 0.0); 
+    glEnd();
+    glPopMatrix();
+
+def Set3DMode():
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(50.0, 640.0 / 480.0, 1, 300);
+    #gluLookAt(0, 0, 400, 0, 0, 0, 0.0, 1.0, 0.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glEnable(GL_TEXTURE_2D) # initialize texture mapping
+    glEnable(GL_LIGHTING);
+
+def DrawGLScene():
+    global X_AXIS,Y_AXIS,Z_AXIS
+    global DIRECTION
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    Set3DMode()
+
+    glTranslatef(0.0+camx,0.0,0.0+camz)
+    glTranslatef(0.0-camx,0.0,0.0-camz)
+    glRotatef(X_AXIS,1.0,0.0,0.0)
+    glRotatef(Y_AXIS,0.0,1.0,0.0)
+    glRotatef(Z_AXIS,0.0,0.0,1.0)
+    glTranslatef(0.0+camx,0.0,-0.0+camz)
+
+    DrawDungeon()
+    DrawBox()
+    DrawSky()
+
+    Set2DMode()
+
+    Draw2DImage(textureBorder1,  172.0,339.0, 640-172.0,480-339.0)
+    Draw2DImage(textureFooter,   483.0,24.0,  0.0,480-109-24+2)
+    Draw2DImage(textureBorder2,  469.0,109.0, 0.0,480-109.0)
+    Draw2DImage(textureBorder3,  468.0,8.0,   0.0,0.0)
+    Draw2DImage(textureBorder4,  8.0,344.0,   0.0,0.0)
+    Draw2DImage(textureMaleA01,  59.0,79.0,   23.0,383.0)
+    Draw2DImage(textureEradcate, 59.0,79.0,   136.0,383.0) # distance ~113px
+    Draw2DImage(textureTap2,     172.0,142.0, 640-172.0,0.0)
+
+    glEnable(GL_DEPTH_TEST);
+    glutSwapBuffers();
+
+def loadTexture (dirname, sfile, texture, transparency_color=None):
     l = lm.GetLod(dirname)
     ret = l.GetFileData("", sfile)
-    
-    width  = ret['img_size'][0]
-    height = ret['img_size'][1]
-    
-    img = Image.new("P", ret['img_size'])
-    img.putdata(ret['data'])
-    img.putpalette(ret['palette'])
+    width  = 0
+    height = 0
+    if ret.get('img_size') is not None:
+        width  = ret['img_size'][0]
+        height = ret['img_size'][1]
+        
+        img = Image.new("P", ret['img_size'])
+        img.putdata(ret['data'])
+        img.putpalette(ret['palette'])
+    else:
+        fdata = io.BytesIO(ret['data'])
+        img = Image.open(fdata)
+        width = img.size[0]
+        height = img.size[1]
     img = img.convert("RGBX")
-    image  = img.tostring ( "raw", "RGBX", 0, -1 )
+    
+    if transparency_color is not None:
+        t=transparency_color
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                if set(img.getpixel((x, y))) == set((t[0], t[1], t[2], 255)):
+                    img.putpixel((x, y), (t[0], t[1], t[2], 0))
+    
+    image  = img.tostring("raw", "RGBX", 0, -1)
+            
     #texture = glGenTextures ( 1 )
     glBindTexture     ( GL_TEXTURE_2D, texture )   
     glPixelStorei     ( GL_UNPACK_ALIGNMENT,1 )
     glTexParameterf   ( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT )
     glTexParameterf   ( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT )
-    glTexParameteri   ( GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR )
-    glTexParameteri   ( GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR )
+    glTexParameteri   ( GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR ) #NEAREST
+    glTexParameteri   ( GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST_MIPMAP_LINEAR ) #any combo
     gluBuild2DMipmaps ( GL_TEXTURE_2D, 3, width, height, GL_RGBA, GL_UNSIGNED_BYTE, image )
 
     return texture
@@ -274,7 +361,7 @@ def main():
     global ID 
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-    glutInitWindowSize(800,600)
+    glutInitWindowSize(640,480)
     glutInitWindowPosition(200,200)
 
     window = glutCreateWindow(b'OpenGL Python Maze Test')
@@ -283,16 +370,29 @@ def main():
     lm = LodManager()
     lm.LoadLods('data')
 
-    loadTexture ( "bitmaps", "bemob2b", textureFloor )
-    loadTexture ( "bitmaps", "d2ceil4", textureCeil )
-    loadTexture ( "bitmaps", "bcsctr", textureWall )
-    loadTexture ( "bitmaps", "cbsm", textureCube )
-    loadTexture ( "bitmaps", "sky07", textureSky)
+    loadTexture ("bitmaps", "bemob2b", textureFloor)
+    loadTexture ("bitmaps", "d2ceil4", textureCeil)
+    loadTexture ("bitmaps", "bcsctr", textureWall)
+    loadTexture ("bitmaps", "cbsm", textureCube)
+    loadTexture ("bitmaps", "sky07", textureSky)
 
+    loadTexture ("icons", "border1.pcx", textureBorder1)
+    loadTexture ("icons", "border2.pcx", textureBorder2)
+    loadTexture ("icons", "border3", textureBorder3)
+    loadTexture ("icons", "border4", textureBorder4)
+    loadTexture ("icons", "eradcate", textureEradcate)
+    loadTexture ("icons", "malea01", textureMaleA01)
+    loadTexture ("icons", "tap2", textureTap2, (255,0,0))
+    loadTexture ("icons", "footer", textureFooter)
+    
     glutDisplayFunc(DrawGLScene)
     glutIdleFunc(DrawGLScene)
     glutKeyboardFunc(keyPressed)
-    InitGL(800, 600)
+    InitGL(640, 480)
+
+    t = threading.Thread(target=threadInc)
+    t.daemon = True
+    t.start()
 
     glutMainLoop()
  
